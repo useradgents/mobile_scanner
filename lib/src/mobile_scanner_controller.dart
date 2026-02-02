3c0e567690c39eb9d90394ebd1ef2c5d4270d8b0
Jeshanth Jeyachelvan <j.jeyachelvan@useradgents.com>
2026-02-02 12:15:31 +0100

    fix : fix scan orientation

--- lib/src/mobile_scanner_controller.dart
+++ lib/src/mobile_scanner_controller.dart
@@ -147,7 +147,7 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
   StreamSubscription<BarcodeCapture?>? _barcodesSubscription;
   StreamSubscription<TorchState>? _torchStateSubscription;
   StreamSubscription<double>? _zoomScaleSubscription;
-  StreamSubscription<DeviceOrientation>? _deviceOrientationSubscription;
+  // StreamSubscription<DeviceOrientation>? _deviceOrientationSubscription;
 
   bool _isDisposed = false;
   // This completer keeps track of whether the MobileScanner widget,
@@ -159,12 +159,12 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
     unawaited(_barcodesSubscription?.cancel());
     unawaited(_torchStateSubscription?.cancel());
     unawaited(_zoomScaleSubscription?.cancel());
-    unawaited(_deviceOrientationSubscription?.cancel());
+ //   unawaited(_deviceOrientationSubscription?.cancel());
 
     _barcodesSubscription = null;
     _torchStateSubscription = null;
     _zoomScaleSubscription = null;
-    _deviceOrientationSubscription = null;
+   // _deviceOrientationSubscription = null;
   }
 
   void _setupListeners() {
@@ -206,7 +206,7 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
           value = value.copyWith(zoomScale: zoomScale);
         });
 
-    if (MobileScannerPlatform.instance
+   /* if (MobileScannerPlatform.instance
         case final MethodChannelMobileScanner implementation
         when defaultTargetPlatform != TargetPlatform.macOS) {
       _deviceOrientationSubscription = implementation
@@ -218,7 +218,7 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
 
             value = value.copyWith(deviceOrientation: orientation);
           });
-    }
+    }*/
   }
 
   void _throwIfNotInitialized() {
